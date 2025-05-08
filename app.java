4f1364721b9700702ee7a962c7d6de2fb111674d
MinhQuang425 <auemyulw@gmail.com>
2025-05-08 08:12:46 +0700

    Update app.java

--- app.java
+++ app.java
@@ -1,1 +1,13 @@
-rwểw
+public class Number {
+ public int i;
+}
+public static void main(String[] args) throws Exception {
+Number n1 = new Number();
+Number n2 = new Number();
+n1.i = 2;
+n2.i= 5;
+n1 = n2;
+n2.i = 10; // what is n1.i?
+System.out.println(n2.i);//10 or 20
+}
+}
